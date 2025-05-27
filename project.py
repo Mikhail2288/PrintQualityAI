7018343bd4a1fbc3ff96c6da635f5e0380f076d7
mishacode <mifanyamalyshev@gmail.com>
2025-05-27 18:30:06 +0500

    йщ

---
+++ project.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import streamlit as st
+from PIL import Image
+import io
+
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+def load_model():
+    model = EfficientNetB0(weights='imagenet')
+    return model
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение', type=["jpg", "png"])
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+model = load_model()
+
+st.title('Классификация изображений')
+img = load_image()
+result = st.button('Распознование изображения')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознования:**')
+    print_predictions(preds)
